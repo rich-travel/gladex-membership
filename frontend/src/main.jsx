43c5a0d8a4e42e2bf1ab745b6adff0801ae9a7f6
rich <richener.travel@gmail.com>
2024-12-12 18:31:39 +0800

    adjust routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import router from './routers/Router.jsx'
+import router from './routers/router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
