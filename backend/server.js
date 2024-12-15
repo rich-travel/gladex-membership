17ad8d56835df18c5e390c1f5f72b340eb3b73b8
rich <richener.travel@gmail.com>
2024-12-15 19:01:37 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,16 @@ const port = process.env.PORT || 4000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin:
-      process.env.CURRENT_ENV === "development"
-        ? process.env.FRONTEND_URL
-        : process.env.FRONTEND_URL_PROD,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin:
+//       process.env.CURRENT_ENV === "development"
+//         ? process.env.FRONTEND_URL
+//         : process.env.FRONTEND_URL_PROD,
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
