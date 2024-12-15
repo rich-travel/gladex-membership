6fb93ab5bcb8152c4ec57c26a0dda61ecfe88c10
rich <richener.travel@gmail.com>
2024-12-15 19:51:54 +0800

    change api connection

--- frontend/src/components/Modals/ChangeAdminModal/ChangeAdminModal.jsx
+++ frontend/src/components/Modals/ChangeAdminModal/ChangeAdminModal.jsx
@@ -1,8 +1,8 @@
 import { UserOutlined } from "@ant-design/icons";
 import { Button, Form, Input, Modal } from "antd";
-import axios from "axios";
 import { useState } from "react";
 import Swal from "sweetalert2";
+import api from "../../../services/api";
 import useChangeAdminModalStore from "../../../stores/changeAdminModalStore";
 
 export default function ChangeAdminModal() {
@@ -22,9 +22,7 @@ export default function ChangeAdminModal() {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.put(
-        `${import.meta.env.VITE_BACKEND_URL}/api/users/${membershipId}`
-      );
+      const response = await api.put(`/api/users/${membershipId}`);
       console.log("User updated to admin:", response);
 
       Swal.fire({
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 // Create an Axios instance
+const env = import.meta.env.VITE_ENV;
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKEND_URL}/`, 
+  baseURL:
+    env === "development"
+      ? `${import.meta.env.VITE_BACKEND_URL}/`
+      : `${import.meta.env.VITE_BACKEND_URL_PROD}/`,
   headers: {
     "Content-Type": "application/json",
   },
